51ef2defecd081ef063dff633f7627be491c1eb4
Sameer Ranjan <sameerranjan090@gmail.com>
2026-03-28 21:07:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,10 @@
 #  Algorithm: Linear Regression (scikit-learn)
 # ============================================================
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
